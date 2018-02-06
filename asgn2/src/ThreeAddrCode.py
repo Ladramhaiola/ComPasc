715c552594a892b2b160f7452214edabb1a0a583
Rohit Bose <rohit.kr.bose@gmail.com>
2018-02-06 14:03:29 +0530

    add nonsense changes

--- asgn2/src/ThreeAddrCode.py
+++ asgn2/src/ThreeAddrCode.py
@@ -24,31 +24,31 @@ class ThreeAddrCode:
         print ("=========================================")
 
         for code in self.code:
-            lineno, op, lhs, op1, op2 = code
+            lineno, op, op3, op1, op2 = code
 
             if op == '=':
-                print (lineno, '\t', lhs, '<-', op1)
+                print (lineno, '\t', op3, '<-', op1)
             if op == '+':
-                print (lineno, '\t', lhs, '<-', op1, op, op2)
+                print (lineno, '\t', op3, '<-', op1, op, op2)
             if op == '-':
-                print (lineno, '\t', lhs, '<-', op1, op, op2)
+                print (lineno, '\t', op3, '<-', op1, op, op2)
             if op == '*':
-                print (lineno, '\t', lhs, '<-', op1, op, op2)
+                print (lineno, '\t', op3, '<-', op1, op, op2)
             if op == '/':
-                print (lineno, '\t', lhs, '<-', op1, op, op2)
+                print (lineno, '\t', op3, '<-', op1, op, op2)
             if op == '**':
-                print (lineno, '\t', lhs, '<-', op1, op, op2)
+                print (lineno, '\t', op3, '<-', op1, op, op2)
             if op == 'ret':
                 print (lineno, '\t', op)
             if op == 'goto':
-                # LHS holds the line number to go to
-                print (lineno, '\t', op, lhs)
+                # op3 holds the line number to go to
+                print (lineno, '\t', op, op3)
             if op == 'if':
-                # LHS is the comparison condition
-                print (lineno, '\t', op, op1, lhs, op2)
+                # op3 is the comparison condition
+                print (lineno, '\t', op, op1, op3, op2)
             if op == 'label':
-                # LHS is function name
-                print (lineno, '\t', op, lhs)
+                # op3 is function name
+                print (lineno, '\t', op, op3)
         print ("=========================================")
 
     def addTo3AC (self, listCode):
