751e73fa64870cc484502ab74e65587c75843999
Rohit Bose <rohit.kr.bose@gmail.com>
2018-02-08 02:29:31 +0530

    Making changes to handle_binary in codegen

---
+++ asgn2/src/codegen.py
@@ -0,0 +1,235 @@
+
+class codeGenerator():
+    '''
+        Issues:
+            Transforming Labels
+            Where to add each instruction to? Like how is the basic block interfacing happening?
+        Args:
+            symTab: Symbol Table formed in main.py
+            threeAC: Three AC code formed in main.py
+            varAllocate: the varAllocate object from main.py
+    '''
+
+    def __init__(self,symTab,threeAC,varAllocate, code):
+
+        self.symTab = symTab
+        self.threeAC = threeAC
+        self.asm_code = {}
+        self.curr_func = '' # to know which function we are generating for right now
+        self.varAllocate = varAllocate
+        self.code = threeAC.code
+
+        # Register descriptor
+        self.registerToSymbol = self.varAllocate.registerToSymbol
+
+        # Memory descriptor
+        self.symbolToRegister = self.varAllocate.symbolToRegister # dict with key value pairs.
+        # For a given register, we get a list, whos first element is the register, and second is the memory location
+
+
+        # Redundant, but required now for referencing
+        # self.operator_list = ["unary","jmp","jtrue","jfalse","loadref","storeref","label","param","call","return","returnval"]
+
+        # Operation list for 32 bit registers
+        self.op32_dict = {"+":"add",
+                        "-":"sub",
+                        "*":"imul",
+                        "/":"idiv",
+                        "MOD":"mod",
+                        "OR":"or",
+                        "AND":"and",
+                        "SHL":"shl",
+                        "SHR":"shr"}
+
+    def updateDescriptors(self):
+        '''
+            Update the descriptors as and when required by the algorithm
+        '''
+
+    def StatementType (self, line):
+        # binary arithmetic
+        if (line[1] in op32_dict):
+            if (isinstance(line[3], int) and isinstance(line[4], int)):
+                return ('BA_2C')
+            elif (!isinstance(line[3], int) and isinstance(line[4], int)):
+                return ('BA_1C_R')
+            elif (isinstance(line[3], int) and !isinstance(line[4], int)):
+                return ('BA_1C_L')
+            else
+                return ('BA_V')
+
+
+    ### --------------------------- INDIVIDUAL ASSEMBLY INSTRUCTIONS -------------------- ###
+
+    def handle_binary (self, lineno, op, lhs, op1, op2):
+        '''
+            Does this handle the case when op1 is constant
+            For example a = a + 3? Yes, it will
+        '''
+
+        # GetReg gives a location L to perform Operation, L(loc) is a register (for this assignment)
+        loc, msg = self.varAllocate.getReg() # Need to send things from here. What though?
+
+        line = self.code[lineno]
+        op = self.op32_dict[line[1]]
+        lineno, operator, lhs, op1, op2 = line
+        statTyp = StatementType(line)
+        ascode = ""
+
+        if self.symbolToRegister[op1.name] is not None:
+                loc_op1 = self.symbolToRegister[op1.name] # Fetching register, which is prefered if it exists
+            else:
+                loc_op1 = self.getFromMem(op1.name)
+        if self.symbolToRegister[op2.name] is not None:
+                loc_op2 = self.symbolToRegister[op2.name]
+            else:
+                loc_op2 = self.getFromMem(op2.name)
+
+        if (statTyp == 'BA_2C'):
+            reg = self.varAllocate.getReg(blockIndex, line) 
+            ascode =  op + " $" + op1 + "," + loc + "\n" + "add $" + op2 + "," + loc + "\n"
+        elif (statTyp == 'BA_1C_R'):
+            ascode = op + " $" + op2 + "," + loc + "\n"
+        elif (statTyp == 'BA_1C_L'):
+            ascode = op + " $" + op1 + "," + loc + "\n"
+        else:
+            if (msg == "Replaced op1"):
+                ascode = op + " " + loc_op2 + "," + loc
+            elif (msg == "Replaced op2")
+                ascode = op + " " + loc_op1 + "," + loc
+            elif (msg == "Replaced NetUse"):
+                
+            elif (msg == "Replaced nothing"):
+                ascode = op + " " + loc_op1 + "," + getFromMem(lhs.name) + "\n" + op + " " + loc_op2 + "," + getFromMem(lhs.name) "\n"
+
+
+        
+
+
+        if loc_op1 != loc:
+            self.code += "movb %" + loc_op1 + ", %" + loc + "\n"
+
+        # Get the machine instruction
+        operation = self.op32_dict[op]
+        self.code += operation + " %" + loc_op2 + ", %" + loc + "\n"
+
+
+        ### --- Update descriptors for L and LHS --- ###
+
+        # For L, if it is a register
+        if loc in self.validreglist:
+            self.registerToSymbol[loc] = lhs.name
+        # For lhs, and updating symbol to mem map
+        if loc in self.validreglist:
+            self.symbolToRegister[lhs.name][0] = loc # if it is a register, update the first entry
+        else:
+            self.symbolToRegister[lhs.name][1] = loc # if mem, the second entry
+
+
+        # If op1 and/or op2 have no next use, update descriptors to include this info. [?]
+
+    def handle_jump (self,op1):
+        self.code += "jmp " + op1.name
+
+
+    def handle_jtrue (self,op1,op2):
+
+    def loadref (self):
+
+    def storeref (self):
+
+
+    def handle_funccall (self,op1):
+        '''
+        WARNING: Have to take into account context of function, or global.
+        Not doing that currently.
+        Essentially, self.code needs to be something more elaborate.
+        '''
+        self.code += 'call ' + op1.name + '\n'
+
+    def handle_param(self,op1):
+        self.code += 'push %' + op1.name + '\n'
+
+    def handle_return(self):
+        self.code += 'ret\n'
+
+    def handle_returnval(self,op1):
+        '''
+        Have to look how values are returned
+        '''
+        self.code += '\nret'
+
+
+    ### ---------------------------- AGGREGATORS ---------------------------------------- ###
+
+
+    def setup_text(self):
+        '''
+            text section
+            Refer to 3AC_complete.md for exact 3 Abstract Code definitions
+        '''
+
+        for codeLine in self.threeAC:
+            lineno, op, lhs, op1, op2 = codeLine
+            # lineno, op are NEVER NULL
+            if op == 'unary':
+                self.handle_unary()
+
+            elif op == 'jmp':
+                self.handle_jmp()
+
+            elif op == 'jtrue':
+                self.handle_jtrue()
+
+            elif op == 'jfalse':
+                self.handle_jfalse()
+
+            elif op == 'loadref':
+                self.handle_loadref()
+
+            elif op == 'storeref':
+                self.handle_storeref()
+
+            elif op == 'label':
+                self.handle_label()
+
+            elif op == 'call':
+                self.handle_funccall(op1)
+
+            elif op == 'param':
+                self.handle_param(op1)
+
+            elif op = 'return':
+                self.handle_return()
+
+            elif op == 'returnval':
+                self.handle_returnval(op1)
+
+            else:
+                handle_binary(lineno,op,lhs,op1,op2)
+
+
+    def setup_data(self):
+        '''
+            data section
+            FFT: Just pick stuff from symbol table?
+        '''
+
+        self.asm_code['data'] = []
+        self.asm_code['data'] += '.data'
+
+    def setup_all(self):
+        '''
+            integrate across all the major parts
+        '''
+        self.asm_code += 'section .text\nglobal _start\n\n'
+        self.setup_text()
+        self.setup_data()
+
+    def display_code(self):
+        print ';============================'
+        print ';--------- x86 code ---------'
+        print ';============================'
+        print self.asm_code
+        print ';============================'
+        print ';============================'
---
+++ asgn2/src/varAllocateRegister.py
@@ -0,0 +1,147 @@
+import os
+import sys
+
+class varAllocateRegister:
+    '''
+    Class holding the register allocated and the next use information for a symbol in the given scope
+    '''
+
+    def __init__(self,SymTable,ThreeAddrCode):
+
+        # nextUse maps every basic block to a list of  dictionaries containing next use info for every symbol in the block
+        self.nextUse = []
+        self.registerToSymbol = {}                                       # stores register to symbol mapping
+        self.symbolToRegister = {}                                       # symbol to register mapping
+        self.unusedRegisters = ["eax","ebx","ecx","edx"]
+        self.usedRegisters = []
+        self.SymTable = SymTable
+        self.basicBlocks = []
+        self.leaders = []                                                # This will determine the basic blocks 
+        self.code = ThreeAddrCode.code
+
+    def getBasicBlocks(self):
+        '''
+        Stores the basic blocks as [startline,endline] pairs in the list self.basicBlocks
+        '''
+        code = self.code
+        self.leaders.append(1)                         # first statement is a leader
+        for i in range(len(code)):
+            codeLine = code[i]
+            if codeLine[1] in ["jmp","je","jne","jz","jg","jl","jge","jle"]:
+                self.leaders.append(codeLine[3])       # target of a jump is a leader
+            self.leaders.append(code[i+1][3])          # statement next to a jump statement is a leader
+
+        self.leaders = list(set(self.leaders))         # removes duplicates
+        self.leaders.sort()
+
+        for i in range(len(self.leaders)):
+            self.basicBlocks.append([self.leaders[i],self.leaders[i+1]])
+
+    def blockAssignNextUse(self,blockIndex):
+        '''
+        Reading the code from last line to first line in the given block and updating the next use information.
+        '''
+        self.nextUse[blockIndex] = [];              # This is a list of dictionaries. Each dictionary corresponds to a line
+        block = self.basicBlocks[blockindex]
+        start = block[0]
+        end = block[1]
+        code = self.code[start-1,end]               # Line numbers start from 1 but code list index starts from 0
+
+        for i in range(len(code),-1,-1):
+
+            lineDict = {}
+            codeLine = code[i]
+
+            lhs = codeLine[2]
+            op1 = codeLine[3]
+            op2 = codeLine[4]
+        
+            lhs_symbol = SymTable.lookup(lhs)
+            op1_symbol = SymTable.lookup(op1)
+            op2_symbol = SymTable.lookup(op2)
+        
+            if lhs_symbol.varfunc == "var":
+                lineDict[lhs] = float("inf")
+            if op1_symbol.varfunc == "var":
+                lineDict[op1] = codeLine[0]
+            if op2_symbol.varfunc == "var":
+                lineDict[op2] = codeLine[0]
+
+            self.nextUse[blockIndex].append(lineDict)                        # These dictionaries will be appended in reverse order of the line number
+
+        self.nextUse[blockIndex] = list(reversed(self.nextUse[blockIndex]))
+
+    def iterateOverBlocks(self):
+        '''
+        This is being used to calculate next use line numbers for variables in a basic block
+        '''
+        code = self.code
+
+        for i,block in enumerate(self.basicBlocks):
+            self.blockAssignNextUse(i)
+    
+    def getBlockMaxUse(self,blockIndex): # Mend this function; it should accept line number too
+        '''
+            This returns the symbol with the maximum value of next use in the given basic block
+        '''
+        blockMaxNext = 0
+        blockMaxSymbol = ""
+        blockNextUse = self.nextUse[blockIndex]                              # This is a list of dictionaries
+        for i,lineNextUse in enumerate(blockNextUse):
+            lineMaxSymbol = max(lineNextUse.iterkeys(), key=(lambda key: lineNextUse[key]))
+            lineMaxNext = lineNextUse[lineMaxSymbol]
+            if lineMaxNext > blockMaxNext:
+                blockMaxNext = lineMaxNext
+                blockMaxSymbol = lineMaxSymbol
+
+        return blockMaxSymbol
+
+    def movToMem (self, reg, v):
+    	dataSection
+
+    def getReg(self,blockIndex,line):
+        '''
+            Refer to slide 29, CodeGen.pdf for the cases
+            WARNING: Still not returning anything ?
+        '''
+        reg = ""
+        msg = ""
+        codeLine = self.code[line]
+        
+        lhs = self.SymTable.lookup(codeLine[2]) # x
+        op1 = self.SymTable.lookup(codeLine[3]) # y
+        op2 = self.SymTable.lookup(codeLine[4]) # z
+
+        # x = y OP z
+
+        nextUseInBlock = self.nextUse[blockIndex][line - self.basicBlocks[blockIndex][0]]
+
+        # float("inf") means that variable has no next use after that particular line in the block
+        if ( op1.varfunc == "var" and self.symbolToRegister[op1.name] != "" and nextUseInBlock[op1.name] == float("inf") ):
+            reg = self.symbolToRegister[op1.name]
+            self.symbolToRegister[op1.name] = ""
+            msg = "Replaced op1"
+        elif ( op2.varfunc == "var" and self.symbolToRegister[op2.name] != "" and nextUseInBlock[op2.name] == float("inf") ):
+            reg = self.symbolToRegister[op2.name]
+            self.symbolToRegister[op2.name] = ""
+            msg = "Replaced op2"
+        elif ( len(self.unusedRegisters) > 0 ):
+            reg = self.unusedRegisters[0]
+            self.unusedRegisters.remove(reg)
+            self.usedRegister.append(reg)
+            msg = "Did not replace"
+        elif (( lhs.varfunc == "var" and nextUseInBlock[lhs.name] != float("inf"))):
+        	MU_var = getBlockMaxUse(blockIndex, line)
+        	reg = self.symbolToRegister[MU_var.name]
+        	self.movToMem (reg,MU_var)
+        	msg = "Replaced NextUse"
+        else:
+        	reg = ""
+        	msg = "Replaced Nothing"
+
+        self.registerToSymbol[reg] = lhs.name
+        self.symbolToRegister[lhs.name] = reg
+
+
+
+        return (reg, msg)
