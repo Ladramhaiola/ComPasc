7d9bcf316cd64c361c07ddf05dfa144dddc16ae0
kaartik bhushan <kbhushan@iitk.ac.in>
2018-03-24 15:07:51 +0530

    updated SymTable

--- asgn4/src/SymTable.py
+++ asgn4/src/SymTable.py
@@ -10,47 +10,35 @@ class SymTable (object):
     def __init__(self): # local variables can be inside functions and functions only
 
         # default values
-        self.table = {'Scope' : 'Main',
-                        'ParentScope' : 'Main',
-                        'Type' : 'function', # This can be function or loop
-                        'ReturnType' : 'undefined',
-                        'Func' : {},
-                        'Ident' : {}
-                        }
-        self.scopelist = [self.table] 
+        self.table = {
+            'Main': {
+                'ParentScope' : None,
+                'Type' : 'function', # This can be function or loop
+                'ReturnType' : 'undefined',
+                'Func' : {},
+                'Ident' : {}
+            }
+        }
+        self.currScope = 'Main'
         # add itself, at least! Index 0 has highest, 1 is inside 0, 2 is inside 1 ...
         # Current scope is always at index [-1]
 
     def PrintSymTable(self):
-        pprint.pprint(self.scopelist)
+        pprint.pprint(self.table)
 
     def GetCurrentScopeName(self):
-        '''
-            WARNING: Currently, the Symbol Table has no key with name 'ScopeName'
-        '''
-        return self.scopelist[-1]['ScopeName'] # even if it contains only one scope, this command helps
+        return self.currScope 
 
-    def Check_identifier(self, identifier, index):
-        if index == -1 :
-            return None # exhausted everything
-        t_scope = self.scopelist[index]
-        # if t_scope['Ident'].has_key(identifier):
-        if identifier in t_scope['Ident']:
-            return t_scope['Ident'][identifier]
-        else:
-            return self.Check_identifier(identifier, index-1) # go one level up
-
-    def AddScope (self, scopeName, Type):
-        curr_scope = self.scopelist[-1]
+    def AddScope (self, Type):
+        scopeName = self.newScopeName
         temp_scope = {
-            'Scope' : curr_scope['ScopeName']+ '.'+ scopeName, # main.scope
-            'ParentScope' : curr_scope['ScopeName'],
+            'ParentScope' : self.currScope,
             'Type' : Type, # Type of scope
             'ReturnType' : 'undefined', # default value
             'Func' : {},
             'Ident' : {},
         }
-        self.scopelist.append(temp_scope)
+        self.table[scopeName] = temp_scope
 
     def Define(self, v, typ, varfunc):
         '''
@@ -71,16 +59,27 @@ class SymTable (object):
             # print ('Symbol already exists!')
         return e
 
+    def getScope(self, identifier):
+        scope = self.currScope
+        while scope != None:
+            if identifier in self.table[scope]['Ident']:
+                return scope
+            else:
+                scope = self.table[scope]['ParentScope']
 
+        return None
+                
     def Lookup(self, identifier):
-        '''
-            Call auxiliary function recursively, i.e. go up the hierarchy
-        '''
-        return self.Check_identifier(identifier, len(self.scopelist) - 1);
+        scope = self.getScope(identifier)
 
-    def Del_scope(self, scopeName):
-        del self.scopelist[-1]
+        if scope == None:
+            return False
+        else:
+            return True
 
+    def endScope(self, scopeName):
+        self.currScope = self.table[self.currScope]['ParentScope']
+        
 class SymTableEntry(object):
     '''
     Create a symbol table entry
