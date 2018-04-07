a9270406e003c34f2ea5f093793552a9dc06e69a
kaartik bhushan <kbhushan@iitk.ac.in>
2018-04-07 19:30:14 +0530

    Split test cases

--- asgn4/test/test.if.pas
+++ asgn4/test/test.if.pas
@@ -9,13 +9,16 @@ CONSTANT
 x = 5;
 y = 6;
 z = 7;
-   
-TYPE
-   a1 = ARRAY[x..10,y..10,z..10] OF INTEGER;
 
 BEGIN
 
-   SCAN(a);
-   a1[7,8,9]:=a1[6,7,8]-"YO";
+   IF a1[6]<z THEN
+      BEGIN
+   	 a1[7] := z;
+      END;
+   ELSE
+      BEGIN
+   	 z := a1[6];
+      END;
 
 END;
