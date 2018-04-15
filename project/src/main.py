b57040cd2b8d02f833b6c1fee2aecadae0ce5361
Siddharth Mittal <smittal6@gmail.com>
2018-04-15 17:25:17 +0530

    removed useless reader and dividetoFunc

--- project/src/main.py
+++ project/src/main.py
@@ -83,41 +83,14 @@ def modify3ACforCodegen(symTab,tac,listCode):
                 temp[4] = op2
                 if representsNum(temp[4]):
                     temp[6] = op2
+            elif (operator == 'CALL'):
+                temp[3] = op1
+            elif (operator == 'PARAM'):
+                temp[3] = op1
 
             newCode.append(temp) # Storing it to the global code store
         tac.code = newCode
 
-def reader (tacf): # 3-addr code file
-	f = open(tacf).readlines()
-	content = [x.strip() for x in f]
-	content = [x.split(',') for x in content]
-	for i in range (len(content)):
-		content[i] = [x.strip() for x in content[i]]
-		while (len(content[i]) != 5):
-			content[i].append('')
-	return content
-
-def divideToFunctions (ac3code):
-	FB = {}
-	flag = 0
-        name = ''
-	for i in range(len(ac3code)):
-		codeline = ac3code[i]
-		#print (codeline)
-		if (codeline[1] == 'LABEL' and codeline[2] == 'FUNC'):
-                        name = codeline[3]
-			if (flag == 0):
-				FB['main'] = [1,i]
-				flag = 1
-			for j in range (i,len(ac3code)):
-				if (ac3code[j][1] == 'RETURN' and ac3code[j][4] == name):
-					break
-                        FB[name] = [i+1,j+1]
-			i = j + 1
-	if (flag == 0):
-                # print ('Value of i',i)
-		FB['main'] = [1,i+1]
-	return FB
 
 def main():
 
@@ -128,10 +101,10 @@ def main():
         tac.display_code()
         modify3ACforCodegen(symTab,tac,tac.code)
 
-	FB = divideToFunctions(tac.code)
+	# FB = divideToFunctions(tac.code)
 	regAlloc = varAllocateRegister(symTab,tac)
 
-    	codeGen = CodeGenerator(symTab, tac, regAlloc, FB)
+    	codeGen = CodeGenerator(symTab, tac, regAlloc)
     	codeGen.setup_all()
     	codeGen.display_code()
 
